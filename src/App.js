d5a028b4ea15a44b00e7a0f0ecb15165e73074e8
julindramarkus <julindramarkus11@gmail.com>
2017-11-01 16:18:18 +0700

    revision

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 export default () => {
   const hallo = "HalLo Moto";
-  const test = "I love react";
   return (
     <div>
     <p>{hallo}</p>
-    <p>{test}</p>
     </div>
 )
 }
